a64fff6104ac2afb549605ce8e9a4f54ed2e8825
NINJA-1029 <sudharsan1602@gmail.com>
2026-06-28 21:22:51 +0530

    chore: update face model downloader for ssd_mobilenetv1

--- backend/downloadModels.js
+++ backend/downloadModels.js
@@ -10,7 +10,10 @@ const modelsToDownload = [
   'face_landmark_68_model-shard1',
   'face_recognition_model-weights_manifest.json',
   'face_recognition_model-shard1',
-  'face_recognition_model-shard2'
+  'face_recognition_model-shard2',
+  'ssd_mobilenetv1_model-weights_manifest.json',
+  'ssd_mobilenetv1_model-shard1',
+  'ssd_mobilenetv1_model-shard2'
 ];
 
 const destDir = path.join(process.cwd(), '../frontend/public/models');
